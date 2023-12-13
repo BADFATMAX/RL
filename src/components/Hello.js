a079e6472df34dc828cb5981a84e27c31b459e13
Alex <a.shovkoplyas@g.nsu.ru>
2023-12-13 13:25:05 +0700

    server fastapi init

--- src/components/Hello.js
+++
@@ -1,9 +0,0 @@
-/*
-function Hello() {
-    return <h1>Hello, welcome to the great Reinforcement learning project! </h1>
-}
-*/
-
-const Hello=() => <h1>Hello, welcome to the great Reinforcement learning project! </h1>
-
-export default Hello;
--- src/components/Message.js
+++
@@ -1,8 +0,0 @@
-import { Component } from "react";
-class Message extends Component {
-    render() {
-        return <h1>This is a component class</h1>
-    }
-}
-
-export default Message;
