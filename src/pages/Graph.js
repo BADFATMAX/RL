7bb9bc22c18f5864b088f1143cf1f53583ed57af
cbipok930 <alex_andr_6@mail.ru>
2023-12-28 02:20:34 +0700

    download

--- src/pages/Graph.js
+++ src/pages/Graph.js
@@ -16,14 +16,37 @@ async function getGraph(){
     }
     return ret;
 }
+
+function getBtn(){
+  async function handler(){
+    console.log("handler");
+    try{
+      let resp = await fetch("http://127.0.0.1:8000/ex_download");
+      let blob = await resp.blob();
+      let file = window.URL.createObjectURL(blob);
+      window.location.assign(file);
+      console.log(resp);
+    }
+    catch (er){
+      alert(er);
+    }
+  }
+  let ret = <></>
+  if (sessionStorage.getItem("INITED")){
+    return <div><button onClick={handler}>Download</button></div>;
+  }
+}
 export default function Graph() {
   const [graph, setGraph] = useState();
+  // const [downloadBt, setdwnBt] = useState();
   getGraph().then((res) => setGraph(res));
+  // setdwnBt(getBtn());
   return (
     <div className="Graph">
       <h1>Visualization and results</h1>
       {/* <button onClick={handleButtonClick}>Go to Stop</button> */}
       {graph}
+      {getBtn()}
     </div>
   );
 }
